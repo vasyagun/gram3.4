80e769a8d4f76d2c11f7c33564acf4e69c6b6062
vasyagun <63818342+vasyagun@users.noreply.github.com>
2024-02-02 21:42:32 +0300

    Add files via upload

---
+++ start0.bat
@@ -0,0 +1,5 @@
+:_minerstart
+node send_universal.js --api tonhub --gpu 0 --givers 1000 --timeout 3
+goto _minerstart
+
+pause
---
+++ start1.bat
@@ -0,0 +1,5 @@
+:_minerstart
+node send_universal.js --api tonapi --gpu 1 --givers 1000 --timeout 3
+goto _minerstart
+
+pause
